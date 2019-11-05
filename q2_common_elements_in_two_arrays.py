4e9266b058ea3f5126777a7d0ec995a170968a3c
Kai Xiao <show-key@qq.com>
2019-11-05 13:55:03 +0800

    2nd version, to avoid using 'in' which actually iterates list, to accelerate

--- q2_common_elements_in_two_arrays.py
+++ q2_common_elements_in_two_arrays.py
@@ -2,24 +2,22 @@
 def common_elements_in_two_arrays(lst1, lst2):
     if (not lst1) or (not lst2):
         return []
-    long_list, short_list = [], []
-    if len(lst1) > len(lst2):
-        long_list = lst1
-        short_list = lst2
+    result = []
+    p1, p2 = 0, 0
+    while p1 < len(lst1) and p2 < len(lst2):
 
-    else:
-        long_list = lst2
-        short_list = lst1
-
-    return_list = []
-    for item in long_list:
-        if item in short_list:
-            return_list.append(item)
-    return return_list
-
+        if lst1[p1] == lst2[p2]:
+            result.append(lst1[p1])
+            p1 += 1
+            p2 += 1
+        elif lst1[p1] > lst2[p2]:
+            p2 += 1
+        else:
+            p1 += 1
+    return result
 
 
 A=[1,3,4,6,7,9]
-B=[1,2,4,5,9,10,11]
+B=[1,2,4,5,9,10,12]
 
 print(common_elements_in_two_arrays(A, B))
